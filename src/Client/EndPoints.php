adef362018a055d3437475d1e522a45b22a07614
Fernando Lira <local@fernand.us>
2021-12-27 23:15:23 -0300

    [LP-1] added all documented endpoints

--- src/Client/EndPoints.php
+++ src/Client/EndPoints.php
@@ -43,4 +43,72 @@ class EndPoints
 
     const CHARGEBACKS_ID = '/v1/chargebacks/%s';
 
+    const CUSTOMERS = '/v1/customers';
+
+    const CUSTOMERS_SEARCH = '/v1/customers/search';
+
+    const CUSTOMERS_SEARCH_PARAMS = [
+        'email',
+    ];
+
+    const CUSTOMERS_ID = '/v1/customers/%s';
+
+    const CUSTOMERS_ID_CARDS = '/v1/customers/%s/cards';
+
+    const CUSTOMERS_ID_CARDS_ID = '/v1/customers/%s/cards/%s';
+
+    const STORES_ID = '/stores/%s';
+
+    const USERS_ID_STORES = '/users/%s/stores';
+
+    const USERS_ID_STORES_SEARCH = '/users/%s/stores/search';
+
+    const USERS_ID_STORES_SEARCH_PARAMS = [
+        'external_id',
+    ];
+
+    const USERS_ID_STORES_ID = '/users/%s/stores/%s';
+
+    const POS =  '/pos';
+
+    const POS_PARAMS = [
+        'external_id',
+        'external_store_id',
+        'store_id',
+        'category',
+    ];
+
+    const POS_ID = '/pos/%s';
+
+    const IN_STORE_ORDERS = '/mpmobile/instore/qr/%s/%s';
+
+    const IN_STORE_ORDERS_V2 = '/instore/qr/seller/collectors/%s/stores/%s/pos/%s/orders';
+
+    const IN_STORE_ORDERS_ID_V2 = '/instore/qr/seller/collectors/%s/pos/%s/orders';
+
+    const ORDERS = '/merchant_orders';
+
+    const ORDERS_SEARCH = '/merchant_orders/search';
+
+    const ORDERS_SEARCH_PARAMS = [
+        'status',
+        'preference_id',
+        'application_id',
+        'payer_id',
+        'sponsor_id',
+        'external_reference',
+        'site_id',
+        'marketplace',
+        'date_created_from',
+        'date_created_to',
+        'last_updated_from',
+        'last_updated_to',
+        'items',
+        'limit',
+        'offset',
+    ];
+
+    const ORDERS_ID = '/merchant_orders/%s';
+
+    const PREAPPROVAL = '/preapproval';
 }
